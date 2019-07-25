679fa4b3d819a0faabf8d7bdb1c408b1edabef42
seqi <tomseqi@live.co.uk>
2019-07-25 19:27:00 +0100

    Persist prompt even if in subdir

--- firebase.plugin.zsh
+++ firebase.plugin.zsh
@@ -2,48 +2,55 @@ function firebase_project() {
 	if [[ $(is_firebase_project) ]]
 	then		
 		# Check the global firebase config file as priority
-		local config_project_id=$(get_firebase_config_project_id)
+		local config_project_id=$(get_config_project_id)
 		if [[ -n $config_project_id ]]
 		then
 			echo "$config_project_id "
 
 		# If nothing in the firebase config file, use the default value set in .firebaserc
 		else
-			local firebaserc_project_id=$(get_firebaserc_project_id "default")
+			local firebaserc_project_id=$(get_rc_project_id "default")
 			echo "$firebaserc_project_id "
 		fi
 	fi	
 }
 
 function is_firebase_project() {
-	if [[ -e firebase.json ]]
+	# Keep checking up, we may be in a subdir
+	local firebase_dir=$(get_firebase_dir)
+	if [[ -n $firebase_dir ]]
 	then
 		echo 1
 	fi
 }
 
-########## Config File ##########
-function get_firebase_config_project_id() {
-	if [[ $(firebase_config_exists) ]]
-	then
-		local config_project_id=$(get_firebase_config_project)
-		echo "$config_project_id"
-	fi
+function get_firebase_dir() {
+	local dir="$(pwd)"
+	
+	while [[ $dir != '/' ]]
+	do
+			local target="$dir/firebase.json"
+
+			if [[ -e $target ]]
+			then
+					echo $dir
+					break
+			else
+					dir=$(dirname $(realpath $dir))
+			fi
+	done
 }
 
-function firebase_config_exists() {
+function get_config_project_id() {
 	if [[ -e ~/.config/configstore/firebase-tools.json ]]
 	then
-		echo 1
-	fi	
+		# May be either the project id itself or an alias (which lives in .firebaserc)
+		local target=$(get_firebase_dir)
+    	echo $(grep -s $target ~/.config/configstore/firebase-tools.json | cut -d'"' -f 4)
+	fi
 }
 
-# May be either the project id itself or an alias (which lives in .firebaserc)
-function get_firebase_config_project() {
-	echo $(grep -s $(git rev-parse --show-toplevel) ~/.config/configstore/firebase-tools.json | cut -d'"' -f 4)
-}
-
-########## Firebaserc ##########
-function get_firebaserc_project_id() {
-	echo $(grep $1 .firebaserc | cut -d'"' -f 4)
+function get_rc_project_id() {
+	local rc_path="$(get_firebase_dir)/.firebaserc"
+	echo $(grep $1 $rc_path | cut -d'"' -f 4)
 }
